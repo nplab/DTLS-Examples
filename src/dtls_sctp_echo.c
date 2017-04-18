803fa16a1e85eda33aeb9eec81c467d5fe4f1588
Felix Weinrank <weinrank@bsd2.fh-muenster.de>
2017-04-18 14:48:18 +0200

    error handling improved

--- src/dtls_sctp_echo.c
+++ src/dtls_sctp_echo.c
@@ -531,6 +531,7 @@ void start_client(char *remote_address, char* local_address, int port, int lengt
 	SSL *ssl;
 	BIO *bio;
 	const int on = 1;
+	int retval;
 #ifdef SCTP_EVENT
 	struct sctp_event event;
 	uint16_t event_types[] = {SCTP_ASSOC_CHANGE,
@@ -655,9 +656,37 @@ void start_client(char *remote_address, char* local_address, int port, int lengt
 	if (veryverbose)
 		BIO_dgram_sctp_notification_cb(bio, &handle_notifications, (void*) ssl);
 
-	if (SSL_connect(ssl) < 0) {
-		perror("SSL_connect");
-		printf("%s\n", ERR_error_string(ERR_get_error(), buf));
+	retval = SSL_connect(ssl);
+	if (retval <= 0) {
+		switch (SSL_get_error(ssl, retval)) {
+			case SSL_ERROR_ZERO_RETURN:
+				fprintf(stderr, "SSL_connect failed with SSL_ERROR_ZERO_RETURN\n");
+				break;
+			case SSL_ERROR_WANT_READ:
+				fprintf(stderr, "SSL_connect failed with SSL_ERROR_WANT_READ\n");
+				break;
+			case SSL_ERROR_WANT_WRITE:
+				fprintf(stderr, "SSL_connect failed with SSL_ERROR_WANT_WRITE\n");
+				break;
+			case SSL_ERROR_WANT_CONNECT:
+				fprintf(stderr, "SSL_connect failed with SSL_ERROR_WANT_CONNECT\n");
+				break;
+			case SSL_ERROR_WANT_ACCEPT:
+				fprintf(stderr, "SSL_connect failed with SSL_ERROR_WANT_ACCEPT\n");
+				break;
+			case SSL_ERROR_WANT_X509_LOOKUP:
+				fprintf(stderr, "SSL_connect failed with SSL_ERROR_WANT_X509_LOOKUP\n");
+				break;
+			case SSL_ERROR_SYSCALL:
+				fprintf(stderr, "SSL_connect failed with SSL_ERROR_SYSCALL\n");
+				break;
+			case SSL_ERROR_SSL:
+				fprintf(stderr, "SSL_connect failed with SSL_ERROR_SSL\n");
+				break;
+			default:
+				fprintf(stderr, "SSL_connect failed with unknown error\n");
+				break;
+		}
 		exit(-1);
 	}
 
